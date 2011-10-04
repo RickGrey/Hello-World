50c6bd7df8db0cac6a83ab4eacbea88dc719df7f
Richard Grey <Rick_Grey@Richard-Greys-MacBook-Pro.local>
2011-10-04 14:24:52 -0400

    Sample tweaks to greeting index.

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,6 +1,12 @@
 class GreetingController < ApplicationController
   def index
     @welcome_message = "Welcome to the Hello World Rails application"
+
+    @age = 8
+
+    @table = {"headings" => ["this", "+ this", "= this"],
+    		  "body" => [[1, 1, 2], [1, 2, 3], [1, 3, 4]]
+    		 }
   end
 
 end
