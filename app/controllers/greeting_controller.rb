cf0d8754c13d360c3fa6739089fd87a04b2a9bfb
Richard Grey <Rick_Grey@Richard-Greys-MacBook-Pro.local>
2011-10-04 13:02:23 -0400

    Rails 3.1.0 (Ruby 1.9.2-p180) shell created.

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,6 @@
+class GreetingController < ApplicationController
+  def index
+    @welcome_message = "Welcome to the Hello World Rails application"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'a8c8c0b1c33437065a37964780a830ca39436f8562947922eb2a666dd8aef8b2fcfa5e3062b1c627ebea3064a0d30520849cb0f389ea4fd534282e8dd802f051'
